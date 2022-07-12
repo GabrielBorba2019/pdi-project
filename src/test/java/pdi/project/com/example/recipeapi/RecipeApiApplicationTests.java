b691384b12fb43de50dc9907a7b79d0dd77aba11
Ga-Borba <gabriel.borba@mercadolivre.com>
2022-07-11 22:34:57 -0300

    Project Created

---
+++ src/test/java/pdi/project/com/example/recipeapi/RecipeApiApplicationTests.java
@@ -0,0 +1,11 @@
+package pdi.project.com.example.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
